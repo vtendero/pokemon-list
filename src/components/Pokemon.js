9d4568696372b9faad3ebb181ec6ab3eb7055f37
vtendero <tendero.valle@gmail.com>
2021-02-24 09:36:24 +0100

    add Pokemon component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,5 +1,24 @@
 import '../styles/Pokemon.scss';
 
-
+const Pokemon= (props) => {
+   const pokemonType= props.pokemonProp.types.map((type, index) => {
+    return (
+        <li key= {index}>
+            {type}
+        </li>
+        );
+    });
+        return (
+           <article>
+                <img src={props.pokemonProp.url} 
+                    alt={props.pokemonProp.name}>
+                </img>
+                <h3>{props.pokemonProp.name}</h3>
+                <ul>
+                    {pokemonType}
+                </ul>
+            </article>
+        )
+}
 
 export default Pokemon;
