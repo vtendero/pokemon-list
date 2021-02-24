7bb2205b949618635de018cda5d6b8a780231351
vtendero <tendero.valle@gmail.com>
2021-02-24 09:55:56 +0100

    add content and style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import '../styles/Header.scss';
 
 const Header = () => 
     <>
-        <header className = 'header'>
-            <h1 className = 'header__title'>Mi lista de pokemon</h1>
+        <header className= 'header'>
+            <h1 className= 'header__title'>Mi lista de</h1>
+            <img className= 'header__img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png' alt="Logo de pokemon"></img>
         </header>
     </>
 
