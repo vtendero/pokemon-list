e6b792f5149ddd77a256340e49f5c36c6400ea53
vtendero <tendero.valle@gmail.com>
2021-02-24 10:46:17 +0100

    add style to pokemon

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,18 +3,20 @@ import '../styles/Pokemon.scss';
 const Pokemon= (props) => {
    const pokemonType= props.pokemonProp.types.map((type, index) => {
     return (
-        <li key= {index}>
+        <li key= {index} className='pokemonList__item--listTypes_item'>
             {type}
         </li>
         );
     });
         return (
            <>
-                <img src={props.pokemonProp.url} 
-                    alt={props.pokemonProp.name}>
+                <img className='pokemonList__item--image' 
+                    src={props.pokemonProp.url} 
+                    alt={`imagen de ${props.pokemonProp.name}`}>
                 </img>
-                <h3>{props.pokemonProp.name}</h3>
-                <ul>
+                <h3 className='pokemonList__item--name'>
+                    {props.pokemonProp.name}</h3>
+                <ul className='pokemonList__item--listTypes'>
                     {pokemonType}
                 </ul>
             </>
